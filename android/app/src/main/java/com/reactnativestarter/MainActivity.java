7d06d71e4df9d13836467b4a06e0b073ad593b9f
June Domingo <june@cynder.io>
2016-10-23 16:49:17 +0800

    Add react-native-vector-icons

--- android/app/src/main/java/com/reactnativestarter/MainActivity.java
+++ android/app/src/main/java/com/reactnativestarter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.reactnativestarter;
 
 // import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
