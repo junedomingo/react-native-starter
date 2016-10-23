4a468cdb6053b4f4a630c5ee12c0ab79b85bb9d3
June Domingo <june@cynder.io>
2016-10-23 16:54:28 +0800

    Icons helper

---
+++ src/utils/AppIcons.js
@@ -0,0 +1,32 @@
+/* eslint-disable new-cap */
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const replaceSuffixPattern = /--(active|big|small|very-big)/g;
+const icons = {
+	'ios-film': [30, '#000000'],
+	'ios-film-outline': [30, '#000000']
+};
+
+const iconsMap = {};
+const iconsLoaded = new Promise((resolve, reject) => {
+	new Promise.all(
+		Object.keys(icons).map(iconName =>
+		// IconName--suffix--other-suffix is just the mapping name in iconsMap
+		Ionicons.getImageSource(
+		iconName.replace(replaceSuffixPattern, ''),
+		icons[iconName][0],
+		icons[iconName][1]
+		))
+	).then(sources => {
+		Object.keys(icons)
+		.forEach((iconName, idx) => (iconsMap[iconName] = sources[idx]));
+
+		// Call resolve (and we are done)
+		resolve(true);
+	});
+});
+
+export {
+	iconsMap,
+	iconsLoaded
+};
